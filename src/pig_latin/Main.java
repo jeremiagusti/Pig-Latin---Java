dd0852b24ab41f6b759d916f04cfdb9d7bfa248e
Jeremia Leonard Abdining Gusti <jerry.gusti@gmail.com>
2017-12-06 14:01:28 -0600

    adding prompt

--- src/pig_latin/Main.java
+++ src/pig_latin/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Input a word to be transformed into pig latin form: ");
 		String word = input.nextLine(); // Input the word
 		int wordLen = word.length(); // Find out the length
 		char[] finalWord = new char[wordLen + 2];  // Make char array according to the length
@@ -21,7 +22,7 @@ public class Main {
 		Moving.moveRest(word, wordLen, finalWord, numOfCons);
 		
 		// Displaying the final word
-		System.out.println("The pig latin version of inputted word is: ");
+		System.out.print("The pig latin version of inputted word is: ");
 		Display.displayWord(finalWord);
 		
 
